22636bdc2a8caac18cb2eeeaac9cefcb898fc4ff
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-05-31 12:05:42 -0400

    Obtener el Total

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,10 +1,13 @@
 "use client"
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
 
   const order = useStore((state) => state.order);
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -25,6 +28,11 @@ export default function OrderSummary() {
               item={ item }
             />
           )) }
+
+          <p className= "text-2xl font-black mt-20 text-center">
+            Total a pagar: {''}
+            <span className="text-amber-500 font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
 
