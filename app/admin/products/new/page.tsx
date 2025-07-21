476c20d2ad98ad2e513e6cb13d33f61d846869c3
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-07-21 06:57:40 -0400

    compositions

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,4 +1,5 @@
 import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
 
 
@@ -7,7 +8,9 @@ export default function CreateProductPage() {
     <>
       <Heading>Crear Producto</Heading>
 
-      <AddProductForm />
+      <AddProductForm>
+        <ProductForm />
+      </AddProductForm>
     </>
   )
 }
--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,13 +1,24 @@
-import ProductForm from "./ProductForm";
+'use client'
 
-export default function AddProductForm() {
+export default function AddProductForm({ children }: { children: React.ReactNode }) {
+
+    const handleSubmit = async (formData: FormData) => {
+        // const data = {
+        //     name: formData.get('name'),
+        //     price: formData.get('price'),
+        //     categoryId: formData.get('categoryId'),
+        // }
+        console.log('desde handleSubmit')
+    }
+
     return (
         <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-2xl mx-auto">
             <form
                 className="space-y-5"
-                //action={}
+                action={handleSubmit}
             >
-                <ProductForm />
+                { children }
+
                 <input 
                     type="submit" 
                     className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
