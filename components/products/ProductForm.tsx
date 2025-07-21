9e9a420154eda67c2006ace94724060824fd4b08
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-07-21 06:37:16 -0400

    Inmplementacion de option categories

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,4 +1,12 @@
+import { prisma } from "@/src/lib/prisma"
+
+async function getCategories() {
+    return await prisma.category.findMany()
+}
+
 export default async function ProductForm() {
+
+    const categories = await getCategories()
 
     return (
         <>
@@ -40,7 +48,14 @@ export default async function ProductForm() {
                     name="categoryId"
                 >
                     <option value="">-- Seleccione --</option>
-          
+                    {categories.map(category => (
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >
+                            {category.name}
+                        </option>
+                    ))}
                 </select>
             </div>
         </>
